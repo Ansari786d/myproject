b740f2ac9c08036697f9d314b3c20ef51718bc49
Ansari786d <80029229+Ansari786d@users.noreply.github.com>
2021-03-27 13:40:50 +0530

    Add files via upload
    program to automatic clean the folder using python

--- main.py
+++ main.py
@@ -1,13 +1,52 @@
-import time
-from plyer import notification
+import os
+
+
+def createIfnotexist(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+
+def move(foldername, files):   # it moves the files in folder
+    for file in files:
+        os.replace(file, f"{foldername}/{file}")  # move this file into this folder name
+
 
 if __name__ == "__main__":
-    while True:
 
-        notification.notify(
-            title="Please drink water",
-            message="Water is must for healthy body ! please drink ",
-            app_icon="C:\\Users\\ansar\\pythonprogr\\harry_project//desktopnotification/icon.ico",
-            timeout=12)
+    files = os.listdir()
+    files.remove("main.py")
+    
+    
+    print(files)
 
-        time.sleep(60*60)
+    createIfnotexist('Images')
+    createIfnotexist('Docs')
+    createIfnotexist('Media')
+    createIfnotexist("others")
+
+    imgExts = [".jpg", ".jpeg", ".png"]
+    images = [file for file in files if os.path.splitext(file)[1].lower() in imgExts]
+    # print(images)
+
+    docExts = [".doc", ".txt", ".pdf", ".docx"]
+    docs = [file for file in files if os.path.splitext(file)[1].lower() in docExts]
+    # print(docs)
+
+
+    mediaExt = [".mp4", ".mp3", ".flv"]
+    medias = [file for file in files if os.path.splitext(file)[1].lower() in mediaExt]
+    # print(medias)
+
+    others = []
+
+    for file in files:
+        ext = os.path.splitext(file)[1].lower()
+        if (ext not in mediaExt) and (ext not in docExts) and (ext not in imgExts) and os.path.isfile(file):
+            others.append(file)
+
+    # print(others)
+
+    move("Images", images)  # here folder name is Images and files name is images
+    move("Docs", docs)
+    move("Media", medias)
+    move("Others", others)
